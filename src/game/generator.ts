9b9c727eeebce5738f99a3160dab5a730dfe886e
Carl von Bonin <carl.vonbonin@gmail.com>
2026-02-17 16:28:32 -0500

    Tallgrass generation

--- src/game/generator.ts
+++ src/game/generator.ts
@@ -3,7 +3,13 @@ import { Vec2 } from "../engine/vec2.js";
 import { World } from "../engine/world.js";
 import { Stick } from "./drops.js";
 import { GrassTile, NaturalTile, SandTile, StoneTile, WaterTile } from "./tiles.js";
-import { PalmTree } from "./trees.js";
+import { Bush, PalmTree, Tallgrass } from "./trees.js";
+
+function subTileOffset(): Vec2 {
+    const angle = Math.random() * 2 * Math.PI;
+    const radius = Math.random() * 0.4 + 0.1; // 0.1 to 0.5
+    return new Vec2(Math.cos(angle) * radius, Math.sin(angle) * radius);
+}
 
 export function generateTile(world: World, x: number, y: number): NaturalTile | null {
     let dryness = noise.perlin2(x * 0.1, y * 0.1) / 2 + 0.5;
@@ -19,6 +25,10 @@ export function generateTile(world: World, x: number, y: number): NaturalTile | 
         world.addEntity(new Stick(new Vec2(x + 0.5, y + 0.5)));
     } else if (dryness > 0.3 && Math.random() < 0.05 * (-y / 100)) {
         world.addEntity(new PalmTree(new Vec2(x + 0.5, y + 0.5)));
+    } else if (dryness > 0.2 && Math.random() < 0.04 * (-y / 50)) {
+        world.addEntity(new Bush(new Vec2(x + 0.5, y + 0.5).add(subTileOffset())));
+    } else if (dryness > 0.8 && Math.random() < Math.max(0.5, 0.05 * (-y / 50))) {
+        world.addEntity(new Tallgrass(new Vec2(x + 0.5, y + 0.5).add(subTileOffset())));
     }
 
     if (dryness < 0.2) {
--- src/game/trees.ts
+++ src/game/trees.ts
@@ -1,7 +1,11 @@
 import { Entity, Flipbook, RenderContext, Vec2 } from "../engine/index.js";
+import { getImage } from "./tiles.js";
 
 const fpPalmTree = new Flipbook("/assets/entities/palmtree/palm.png", 2, 0.75);
 const fpPalmTreeCoconuts = new Flipbook("/assets/entities/palmtree/palmcoconut.png", 2, 0.75);
+const fpBush = new Flipbook("/assets/entities/bush.png", 2, 0.75);
+const txTallgrassLong = getImage("/assets/entities/tallgrass/long.png");
+const txTallgrassShort = getImage("/assets/entities/tallgrass/short.png");
 
 export class PalmTree extends Entity {
     hasCoconuts: boolean = true;
@@ -18,5 +22,31 @@ export class PalmTree extends Entity {
 
         const flipbook = this.hasCoconuts ? fpPalmTreeCoconuts : fpPalmTree;
         ctx.drawFlipbook(flipbook, this.position.x - 1, this.position.y - 2, 2, 2);
+    }
+}
+
+export class Bush extends Entity {
+    constructor(position: Vec2) {
+        super(position);
+        this.layer = 1;
+        this.size = new Vec2(1, 1);
+    }
+
+    draw(ctx: RenderContext): void {
+        ctx.drawFlipbook(fpBush, this.position.x - 0.5, this.position.y - 0.5, 1, 1);
+    }
+}
+
+export class Tallgrass extends Entity {
+    isTall: boolean;
+
+    constructor(position: Vec2) {
+        super(position);
+        this.layer = 0;
+        this.isTall = Math.random() < 0.2;
+    }
+
+    draw(ctx: RenderContext): void {
+        ctx.drawImage(this.isTall ? txTallgrassLong : txTallgrassShort, this.position.x - 0.5, this.position.y - 0.5, 1, 1);
     }
 }
